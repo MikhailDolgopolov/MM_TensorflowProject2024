dcd877f5871f4cd01edcbd615cc54d5e8801dd23
MikhailDolgopolov <mishad2304@gmail.com>
2024-03-31 11:11:35 +0300

    TF good_words_2 80% success

---
+++ data_preparation.py
@@ -0,0 +1,46 @@
+import string
+
+import pandas as pd
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+short_stopword_list = "a, an, and, are, as, at, be, but, by, for, if, in, into, is, it, no, not, of, on, or, such, that, the, their, then, there, these, they, this, to, was, will, with".split(
+    ", ")
+# print(f"{len(short_stopword_list)=}")
+
+import requests
+
+mid_list = requests.get(
+    r"https://gist.githubusercontent.com/sebleier/554280/raw/7e0e4a1ce04c2bb7bd41089c9821dbcf6d0c786c/NLTK's%2520list%2520of%2520english%2520stopwords")
+mid_stopword_list = mid_list.text.split("\n")
+# print(f"{len(mid_stopword_list)=}")
+
+long_list = requests.get(r"https://raw.githubusercontent.com/stopwords-iso/stopwords-en/master/stopwords-en.txt")
+long_stopword_list = long_list.text.split("\n")
+# print(f"{len(long_stopword_list)=}")
+
+stopword_lists = [short_stopword_list, mid_stopword_list, long_stopword_list]
+
+import json
+
+
+def parse_data(file):
+    for l in open(file, 'r'):
+        yield json.loads(l)
+
+
+def prepare(df):
+    def count(l1, l2): return sum([1 for x in l1 if x in l2])
+
+    df["punct"] = df["headline"].apply(lambda s: count(s, string.punctuation))
+    df["orig_word_count"] = df["headline"].apply(lambda t: len(t.split()))
+    df["headline"] = df["headline"].str.replace(r"[^\w\s]", '', regex=True)
+    for i in range(len(stopword_lists)):
+        col = f"good_words_{i + 1}"
+        df[col] = df["headline"].apply(
+            lambda x: ' '.join([word for word in x.split() if word not in (stopword_lists[i])]))
+        df[f"percent_{i + 1}"] = df[col].apply(lambda t: len(t.split())) / df["orig_word_count"]
+    df.drop(["orig_word_count", "headline"], axis=1)
+    return df
---
+++ main.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from keras import Sequential, layers
+from keras.regularizers import L2
+from sklearn.metrics import accuracy_score
+
+from data_preparation import parse_data, prepare
+
+data = list(parse_data('Data/SarcasmInNews/Sarcasm_Headlines_Dataset_v2.json'))
+original = pd.DataFrame.from_records(data).drop("article_link", axis=1)
+using_text="good_words_2"
+df = prepare(original)[["is_sarcastic", using_text]]
+
+
+import keras
+import tensorflow as tf
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+print(tf.config.list_physical_devices())
+
+vocab_size = 10000
+
+count_vectorizer = tf.keras.layers.TextVectorization(
+    max_tokens=vocab_size,
+    output_mode='count'
+)
+
+train, test = train_test_split(df, test_size=0.2, stratify=df["is_sarcastic"])
+train, validation = train_test_split(train, test_size=0.2, stratify=train["is_sarcastic"])
+count_vectorizer.adapt(train[using_text].values)
+
+training_dataset = tf.data.Dataset.from_tensor_slices(
+    (train[using_text].values, train['is_sarcastic'].values)).batch(batch_size=32)
+
+validation_dataset = tf.data.Dataset.from_tensor_slices(
+    (validation[using_text].values, validation['is_sarcastic'].values)).batch(batch_size=32)
+model = Sequential([
+    count_vectorizer,
+    layers.Dense(64, activation='relu', kernel_regularizer=L2(1e-3)),
+    layers.Dropout(0.1),
+    layers.Dense(64, activation='relu', kernel_regularizer=L2(1e-3)),
+    layers.Dropout(0.1),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    loss=tf.keras.losses.binary_crossentropy,
+    optimizer=tf.keras.optimizers.Adam(learning_rate=2e-5),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    training_dataset,
+    steps_per_epoch=len(training_dataset),
+    epochs=14,
+    validation_data=validation_dataset,
+    validation_steps=len(validation_dataset)
+)
+
+predictions = model.predict(test[using_text]).round()
+print(accuracy_score(test['is_sarcastic'].values, predictions))
